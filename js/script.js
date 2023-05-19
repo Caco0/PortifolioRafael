2a24c9f43ef269fb0e05b81595eea49c76aeabef
Rafael F Dametto <106121395+Caco0@users.noreply.github.com>
2023-05-19 12:44:45 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,62 @@
+/*==================================================alternar icone navbar==================================================*/
+    let menuIcon = document.querySelector('#menu-icon');
+    let navbar = document.querySelector('.navbar');
+
+    menuIcon.onclick = () => {
+        menuIcon.classList.toggle('bx-x');
+        navbar.classList.toggle('active');
+    };
+
+/*==================================================seleção ativa home==================================================*/
+    let selection = document.querySelectorAll('section');
+    let navlinks = document.querySelectorAll('header nav a');
+
+    window.onscroll = () => {
+    selection.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let heigth = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + heigth) {
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+
+    });
+
+/*==================================================Navbar colada==================================================*/
+    let header = document.querySelector('header');
+
+    header.classList.toggle('colada', window.scrollY > 100);
+    
+/*==================================================remove alternar icone navbar quando clica no link navbar==================================================*/
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+};
+
+/*==================================================rolagem ==================================================*/
+ScrollReveal({ 
+    // reset: true, 
+    distance: '80px',
+    duration: 2000, 
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-conteudo, .cabecalho', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .skills-container, .portifolio-box, .contato form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-conteudo h1, .sobre-imagem', { origin: 'left' });
+ScrollReveal().reveal('.home-conteudo p, .sobre-conteudo', { origin: 'rigth' });
+
+/*================================================== typed js ==================================================*/
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer Jr','Adminitrador de Empresa'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
